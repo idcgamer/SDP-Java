bb9388ea7e1384fdeca600fc110278cae8308289
George Carter <46428450+BossName2@users.noreply.github.com>
2024-02-06 16:18:08 +0000

    Added spring boot api
    Added maven and spring boot configuration files as well as a greetings test for the api

---
+++ rest-service/src/test/java/K2161350/SDP/restservice/SdpRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package K2161350.SDP.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
